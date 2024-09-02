4e006f2d1647b2e8cf56f2895d319cb1760e4691
ramguel <160750177+Ramguel1@users.noreply.github.com>
2024-09-02 11:47:06 -0600

    Update app.js

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ const inicio = () => {
 btnPresupuesto.onclick = () => {
     tPresupuesto = (presupuesto.value);
     localStorage.setItem('presupuesto', tPresupuesto)
-    if (tPresupuesto <=0 && tPresupuesto=="") {
+    if (tPresupuesto <=0 || tPresupuesto=="") {
         Swal.fire({ icon: "error", title: "ERROR", text: "Presupuesto mayor a 0" });
         return;
     }
